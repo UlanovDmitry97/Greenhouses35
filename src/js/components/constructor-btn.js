f472aa1a6ff8ddf23b87260c492c9bf70a7fc71e
Dmitriy <Ulanov97_97@mail.ru>
2022-03-11 15:46:26 +0300

    v 1.6.0 Добавлен каталог клумб, и детальные страницы к ним

--- src/js/components/constructor-btn.js
+++ src/js/components/constructor-btn.js
@@ -1,5 +1,6 @@
 const btnsLong = document.querySelectorAll('.greenhouse-product__constructor-btn-long');
 const btnsStep = document.querySelectorAll('.greenhouse-product__constructor-btn-step');
+const colorBtns = document.querySelectorAll('.greenhouse-product__color');
 
 if (btnsLong) {
   btnsLong.forEach(btn => {
@@ -21,6 +22,18 @@ if (btnsStep) {
       });
 
       this.classList.add('greenhouse-product__constructor-btn-step_active');
+    })
+  })
+}
+
+if (colorBtns) {
+  colorBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+      colorBtns.forEach(btn => {
+        btn.classList.remove('greenhouse-product__color_active');
+      });
+
+      this.classList.add('greenhouse-product__color_active');
     })
   })
 }
