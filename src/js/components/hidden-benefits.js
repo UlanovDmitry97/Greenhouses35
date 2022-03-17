61229ac9a3a7b607c088af1cd37eda72c71c5460
Dmitriy <Ulanov97_97@mail.ru>
2022-03-17 11:32:28 +0300

    v 1.12.0 Добавлен каталог и детальные страницы, комплекта грядок. Добавлена страница акций, статьи

--- src/js/components/hidden-benefits.js
+++ src/js/components/hidden-benefits.js
@@ -2,6 +2,8 @@ const btnVisable = document.querySelector('.advantages-product__more');
 const hiddenInfo = document.querySelector('.product-more-info');
 const btnVisableProduct = document.querySelector('.greenhouse-product__more');
 const hiddenInfoProduct = document.querySelector('.greenhouse-product__more-info');
+const btnVisableArticles = document.querySelector('.article__more');
+const hiddenInfoArticles = document.querySelector('.article__more-info');
 
 if (btnVisable) {
   btnVisable.addEventListener('mouseover', () => {
@@ -24,5 +26,17 @@ if (btnVisableProduct) {
   btnVisableProduct.addEventListener('click', () => {
     hiddenInfoProduct.classList.add('greenhouse-product__more-info_visable');
     btnVisableProduct.classList.add('greenhouse-product__more_hidden');
+  });
+}
+
+if (btnVisableArticles) {
+  btnVisableArticles.addEventListener('mouseover', () => {
+    hiddenInfoArticles.classList.add('article__more-info_visable');
+    btnVisableArticles.classList.add('article__more_hidden');
+  });
+
+  btnVisableArticles.addEventListener('click', () => {
+    hiddenInfoArticles.classList.add('article__more-info_visable');
+    btnVisableArticles.classList.add('article__more_hidden');
   });
 }
